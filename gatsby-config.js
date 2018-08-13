40523b323c95b3b02f7d325e82e22127e5de2152
Alex Ruiz <ruiz.alex0302@gmail.com>
2018-08-13 00:25:04 -0700

    Initialize new Gatsby site

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Byte Lab',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Welcome to Byte Lab.</h1>
+    <p>Welcome to your new Gatsby site.</p>
+  </div>
+)
+
+export default IndexPage
