7ea03de92b68b1bd11644574dbea5281d2ea847f
Alex Ruiz <ruiz.alex0302@gmail.com>
2018-08-15 01:43:33 -0700

    Update site styles and content

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,14 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Welcome to Byte Lab.</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h2>Welcome to Byte Lab.</h2>
+    <p>Our site is under construction. Expect us to be
+        done by August 20th, Monday.</p>
+    <p>In the meantime, since you're here: why not
+        check out our <a href="https://www.instagram.com/bytelaboratory">
+        Instagram page?</a></p>
+    <p>Also important! Join our <a href="https://join.slack.com/t/bytelablol/shared_invite/enQtMjUxNzU5MTY3NDQxLWJhMmM0ZjRkNjQxZDU2ZDE5MTFjZGM3ZDhmYTdlZGZjOWZkNzc2ZWVhY2YyMzE0MDk0MzI0N2RhNTg1YmMyYmU">Slack</a> here! That way, you can talk to each other, and
+  reach the club leaders if you have any questions.</p>
   </div>
 )
 
